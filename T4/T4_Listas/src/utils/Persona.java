4d5cf7eb94b00a2282c5279a2e01b0f3ab8c4a97
user <info@developandsys.com>
2019-02-08 16:03:13 +0100

    di

---
+++ T4/T4_Listas/src/utils/Persona.java
@@ -0,0 +1,30 @@
+package utils;
+
+public class Persona {
+
+    String nombre, apellido;
+    int telefono;
+
+    public Persona(String nombre, String apellido, int telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    @Override
+    public String toString() {
+        return getNombre();
+    }
+}
