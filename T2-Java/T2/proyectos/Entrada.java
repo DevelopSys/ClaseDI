55e9af3f97adec000f85d455f700accfc044e2e7
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2019-10-09 09:05:50 +0200

    Create Entrada.java

---
+++ T2-Java/T2/proyectos/Entrada.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+                ventanaPrincipal.initGUI();
+            }
+        });
+
+     }
+}
