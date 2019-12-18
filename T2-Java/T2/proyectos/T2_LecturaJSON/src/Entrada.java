c281d9032b8217c41eb56167396e447e855f0a3a
develop <developandsys@gmail.com>
2019-12-18 08:53:23 +0100

    json

---
+++ T2-Java/T2/proyectos/T2_LecturaJSON/src/Entrada.java
@@ -0,0 +1,41 @@
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Entrada {
+
+    public static void main(String[] args) throws IOException, JSONException {
+
+        String linkUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=4ef66e12cddbb8fe9d4fd03ac9632f6e&language=en-US&page=1";
+        URL url = new URL(linkUrl);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        BufferedReader lector = new BufferedReader
+                (new InputStreamReader(httpURLConnection.getInputStream()));
+        StringBuilder builder = new StringBuilder();
+        String linea;
+        while ((linea=lector.readLine())!=null){
+            builder.append(linea);
+        }
+
+        JSONObject jsonObject = new JSONObject(builder.toString());
+        JSONArray jsonArray = jsonObject.getJSONArray("results");
+        for (int i=0;i<jsonArray.length();i++){
+            JSONObject object = (JSONObject) jsonArray.get(i);
+            String titulo = object.getString("original_title");
+            String descripcion = object.getString("overview");
+            Pelicula p =new Pelicula(titulo,descripcion);
+            Gson gson = new Gson(jsonObject,Pelicula.class);
+            gson.
+            System.out.println(String.format("Titulo: %s %n Descricion %s %n",titulo,descripcion));
+        }
+
+    }
+}
---
+++ T2-Java/T2/proyectos/T2_LecturaJSON/src/Pelicula.java
@@ -0,0 +1,9 @@
+public class Pelicula {
+
+    String titulo, descripcion;
+
+    public Pelicula(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+}
