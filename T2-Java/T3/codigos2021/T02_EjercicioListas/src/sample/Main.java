ee0df5b4d68f2db46c5aa6f775fdaf0744a24f45
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2020-10-27 13:14:31 +0100

    update

--- T2-Java/T3/codigos2021/T02_EjercicioListas/src/sample/Main.java
+++ T2-Java/T3/codigos2021/T02_EjercicioListas/src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("examen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("calculadora.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
