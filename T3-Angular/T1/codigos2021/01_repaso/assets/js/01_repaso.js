d2402321b2f239ab974202a4bf88f5a888a0da93
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2020-12-03 11:00:40 +0100

    update

---
+++ T3-Angular/T1/codigos2021/01_repaso/assets/js/01_repaso.js
@@ -0,0 +1,42 @@
+// comentarios
+/** 
+ comentarios
+*/
+console.log("Esto es un ejemplo de llamada js externa");
+
+const variableConstanteNumero = 2;
+const variableConstanteLetra = "Letra";
+const variableConstanteFloat = 5.6;
+const variableConstanteBoolean = false;
+const variableConstanteUndefined = undefined;
+const variableConstanteArray = [];
+
+console.log("La constante numero es: " + variableConstanteNumero);
+console.log(`La constante letra es: ${variableConstanteLetra}`);
+
+// tipos number, string , booleam , undefined , NaN
+
+console.log(
+  "El tipo de la constatnte Numero es " + typeof variableConstanteNumero
+);
+console.log(
+  "El tipo de la constatnte Float es " + typeof variableConstanteFloat
+);
+console.log(
+  "El tipo de la constatnte Boolean es " + typeof variableConstanteBoolean
+);
+console.log(
+  "El tipo de la constatnte Letra es " + typeof variableConstanteLetra
+);
+console.log(
+  "El tipo de la constatnte undefined es " + typeof variableConstanteUndefined
+);
+console.log(
+  `El tipo de la constatnte undefined es ${typeof variableConstanteUndefined}`
+);
+
+let variableNumero = 3;
+let variableNumeroDos = 3;
+
+var variableVarNumero = 6;
+var variableVarNumeroDos = 6;
---
+++ T3-Angular/T1/codigos2021/01_repaso/assets/js/02_repaso.js
@@ -0,0 +1,1 @@
+console.log("Esta es una llamada desde otro fichero");
