019da0076865cea456dd25573f62e662bf7b7654
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2020-12-17 10:04:52 +0100

    update

---
+++ T3-Angular/T1/codigos2021/01_repaso/assets/js/08_repaso_dom.js
@@ -0,0 +1,8 @@
+let elementos = document.querySelectorAll("div.container#containerPrincipal");
+//console.log(elementos);
+let elemento = document.querySelector("#inputCol1");
+//console.log(elemento.value);
+
+function botonUnoPulsado() {
+  console.log("El boton uno ha sido pulsado");
+}
