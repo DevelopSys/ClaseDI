391a28a27470331b30dfd486958d616a3876416a
develop <developandsys@gmail.com>
2021-01-19 10:05:34 +0100

    update

---
+++ T3-Angular/T1/codigos2021/02_iniciots/assets/js/index.js
@@ -0,0 +1,8 @@
+// tipado de variables
+var nombre = "Borja";
+var numero = 6;
+var acierto = false;
+var arrayDatos = ["asd", "asd", "asd"];
+var arrayCosas = ["add", false, 3, "s"];
+var arrayNumeros = [1, 2, 3, 4, 5];
+console.log(nombre + " " + numero);
---
+++ T3-Angular/T1/codigos2021/02_iniciots/assets/js/index.ts
@@ -0,0 +1,9 @@
+// tipado de variables
+let nombre: string = "Borja";
+let numero: number = 6;
+let acierto: boolean = false;
+let arrayDatos: string[] = ["asd", "asd", "asd"];
+let arrayCosas: any[] = ["add", false, 3, "s"];
+let arrayNumeros: number[] = [1, 2, 3, 4, 5];
+
+console.log(`${nombre} ${numero}`);
