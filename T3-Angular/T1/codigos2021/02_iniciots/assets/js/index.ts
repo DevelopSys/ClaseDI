43c5106d23fb952d7b6fd76a2a0095f8920f26d3
develop <developandsys@gmail.com>
2021-01-20 15:35:29 +0100

    update

--- T3-Angular/T1/codigos2021/02_iniciots/assets/js/index.ts
+++ T3-Angular/T1/codigos2021/02_iniciots/assets/js/index.ts
@@ -109,3 +109,20 @@ console.log(
 // condicion ? loquepasasiestrue : loqueasasiesfalse
 
 //funcionParamentrosMultiples(false, 1, 2, 3, 4, 5, 6, 7);
+
+interface alumno {
+  nombre: string;
+  apellido: string;
+  edad: number;
+  hobbies: [];
+  caracteristicas: {};
+  mostarDatos();
+}
+
+let alumnoUno = {
+  nombre: "Borja",
+  apellido: "Martin",
+  edad: 30,
+  hobbies: [],
+  mostrarDatos: () => {},
+};
