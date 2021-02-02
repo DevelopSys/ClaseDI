ae3726e9a7d7b96e62758b855a716a4bc5b04bc3
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2021-02-02 19:23:15 +0100

    update

--- T3-Angular/T1/codigos2021/01_iniciojs/assets/js/13_modificaciones_dom.js
+++ T3-Angular/T1/codigos2021/01_iniciojs/assets/js/13_modificaciones_dom.js
@@ -9,5 +9,31 @@ let emailInput = document.querySelectorAll("#exampleInputEmail1");
 let busquedaAnidada = document.querySelectorAll("div.mb-3.form-check");
 //let inputEmail = document.querySelectorAll("input#exampleInputEmail1");
 let inputEmail = document.querySelectorAll("input[type='password']");
+let inputs = document.querySelectorAll("input");
 
-console.log(inputEmail);
+//console.log(inputs);
+inputs.forEach((element) => {
+  //console.log(element.value);
+});
+
+let inputUno = document.querySelector("input");
+//console.log(inputUno);
+
+let boton = document.querySelector("button");
+//console.log(boton);
+
+boton.addEventListener("click", () => {
+  //console.log("El elemento ha sido pulsado");
+  let email = document.querySelector("#inputEmail").value;
+  let nombre = document.querySelector("#inputNombre").value;
+  let pass = document.querySelector("#inputPass").value;
+  let experiencia = document.querySelector("#inputCheck").checked;
+
+  console.log(`${nombre} ${email} ${pass} ${experiencia} `);
+
+  let nodo = document.createElement("li");
+  nodo.classList.add("list-group-item");
+  nodo.innerHTML = `<p> ${nombre} ${email} ${pass}</p>`;
+  document.querySelector("#lista_usuarios").append(nodo);
+  document.querySelector("#lista_usuarios").classList.add("list-group");
+});
