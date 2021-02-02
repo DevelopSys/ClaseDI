8be9a2b81a77662b236a211608c8b3f51613dfab
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2021-02-02 18:48:03 +0100

    update

---
+++ T3-Angular/T1/codigos2021/01_iniciojs/assets/js/13_modificaciones_dom.js
@@ -0,0 +1,13 @@
+console.log(document);
+//console.log(window.history);
+//console.log(window.history);
+//window.open("https://www.marca.com");
+// querySelectorAll --> NodeList
+let divs = document.querySelectorAll("div");
+let container = document.querySelectorAll(".container");
+let emailInput = document.querySelectorAll("#exampleInputEmail1");
+let busquedaAnidada = document.querySelectorAll("div.mb-3.form-check");
+//let inputEmail = document.querySelectorAll("input#exampleInputEmail1");
+let inputEmail = document.querySelectorAll("input[type='password']");
+
+console.log(inputEmail);
