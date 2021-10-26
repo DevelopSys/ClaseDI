95840755a457baa8287c5d98b0932f5070d72bed
Develop and System <developandsystem@gmail.com>
2021-10-26 10:06:14 +0200

    update

---
+++ T2-Java/T3/codigos2122/T_Repaso/src/main/java/com/example/t_repaso/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.t_repaso;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+@FXML
+    Button botonUno, botonDos, botonTres;
+}
---
+++ T2-Java/T3/codigos2122/T_Repaso/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.t_repaso {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.t_repaso to javafx.fxml;
+    exports com.example.t_repaso;
+}
