2e5e9ce0f8a0e268daee68ebbeed6340daa60481
Develop and System <developandsystem@gmail.com>
2021-11-02 16:54:26 +0100

    update

--- T2-Java/T3/codigos2122/T_Repaso/src/main/java/com/example/t_repaso/HelloController.java
+++ T2-Java/T3/codigos2122/T_Repaso/src/main/java/com/example/t_repaso/HelloController.java
@@ -1,15 +1,24 @@
 package com.example.t_repaso;
 
+import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,12 +28,23 @@ public class HelloController implements Initializable, EventHandler<Event> {
     @FXML
     Button botonUno, botonDos, botonTres;
 
+    @FXML
+    TextField textoInicial;
+
     DropShadow dropShadow;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instancias();
+        personalizarBotones();
         acciones();
+    }
+
+    private void personalizarBotones() {
+        botonUno.setBackground(null);
+        botonUno.setBorder(null);
+        botonUno.setGraphic(new ImageView(new Image(HelloApplication.class.getResourceAsStream("playg.png"))));
+
     }
 
     private void instancias() {
@@ -32,16 +52,47 @@ public class HelloController implements Initializable, EventHandler<Event> {
     }
 
     public void acciones() {
-        botonUno.setOnAction(this);
+        /*botonUno.setOnAction(this);
         botonDos.setOnAction(this);
         botonTres.setOnAction(this);
-        botonUno.setOnMouseEntered(new ManejoMouse());
-        botonUno.setOnMouseExited(new ManejoMouse());
+        botonUno.setOnMouseEntered(this);
+        botonUno.setOnMouseExited(this);*/
+        botonUno.addEventHandler(MouseEvent.MOUSE_ENTERED, this);
+        botonUno.addEventHandler(MouseEvent.MOUSE_EXITED, this);
+        botonDos.addEventHandler(MouseEvent.MOUSE_ENTERED, this);
+        botonDos.addEventHandler(MouseEvent.MOUSE_EXITED, this);
+        botonTres.addEventHandler(MouseEvent.MOUSE_ENTERED, this);
+        botonTres.addEventHandler(MouseEvent.MOUSE_EXITED, this);
+        botonUno.addEventHandler(ActionEvent.ACTION, this);
+        botonDos.addEventHandler(ActionEvent.ACTION, this);
+        botonTres.addEventHandler(ActionEvent.ACTION, this);
+        textoInicial.addEventHandler(KeyEvent.KEY_RELEASED,this);
+
     }
 
     @Override
     public void handle(Event event) {
 
+        if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
+
+            ((Button)event.getSource()).setEffect(dropShadow);
+            ((Button)event.getSource()).setGraphic(new ImageView(new Image(HelloApplication.class.getResourceAsStream("playr.png"))));
+
+        } else if (event.getEventType() == MouseEvent.MOUSE_EXITED) {
+            ((Button)event.getSource()).setGraphic(new ImageView(new Image(HelloApplication.class.getResourceAsStream("playg.png"))));
+
+            ((Button)event.getSource()).setEffect(null);
+        } else if (event.getEventType() == ActionEvent.ACTION) {
+
+            System.out.println("Pulsado " + ((Button)event.getSource()).getText());
+        } else if (event.getEventType() == KeyEvent.KEY_RELEASED){
+            System.out.println(((KeyEvent)event).getText());
+            //System.out.println("Elemento");
+            if (textoInicial.getText().length()>10){
+                //System.out.println(((KeyEvent)event).getText());
+                event.consume();
+            }
+        }
     }
 
 
@@ -53,7 +104,7 @@ public class HelloController implements Initializable, EventHandler<Event> {
                 if (mouseEvent.getEventType() == MouseEvent.MOUSE_ENTERED) {
                     botonUno.setEffect(dropShadow);
 
-                } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED){
+                } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED) {
                     botonUno.setEffect(null);
                 }
             }
