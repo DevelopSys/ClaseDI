106ae359b550f602457690ac0dd057b17e759a8b
Develop and System <developandsystem@gmail.com>
2021-11-26 19:50:12 +0100

    update

---
+++ T2-Java/T4/codigos2122/LIstas/src/main/java/com/example/listas/ListaApplication.java
@@ -0,0 +1,23 @@
+package com.example.listas;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ListaApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ListaApplication.class.getResource("listas-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Listas");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
