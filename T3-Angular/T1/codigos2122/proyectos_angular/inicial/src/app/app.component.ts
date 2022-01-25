a1bf468eaefa7a645d6c672c0acf7e47905623fc
Develop and System <developandsystem@gmail.com>
2022-01-25 09:00:14 +0100

    update

---
+++ T3-Angular/T1/codigos2122/proyectos_angular/inicial/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'inicial';
+  numero = 5;
+}
