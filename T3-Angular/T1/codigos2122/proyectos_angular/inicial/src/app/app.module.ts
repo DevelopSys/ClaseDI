520791e5e78ace59087a16a4e219ea357026bf56
Develop and System <developandsystem@gmail.com>
2022-01-25 11:42:39 +0100

    update

--- T3-Angular/T1/codigos2122/proyectos_angular/inicial/src/app/app.module.ts
+++ T3-Angular/T1/codigos2122/proyectos_angular/inicial/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondComponent } from './components/second/second.component';
+import { ThirdComponent } from './components/third/third.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SecondComponent, ThirdComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ T3-Angular/T1/codigos2122/proyectos_angular/inicial/src/app/components/third/third.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'myapp-third',
+  templateUrl: './third.component.html',
+  styleUrls: ['./third.component.css']
+})
+export class ThirdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
