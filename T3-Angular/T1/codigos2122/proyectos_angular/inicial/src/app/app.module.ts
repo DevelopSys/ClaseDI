c842e7bfcb8bdade31e155821da0f1c8d82bac67
Develop and System <developandsystem@gmail.com>
2022-01-25 12:10:27 +0100

    update

--- T3-Angular/T1/codigos2122/proyectos_angular/inicial/src/app/app.module.ts
+++ T3-Angular/T1/codigos2122/proyectos_angular/inicial/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecondComponent } from './components/second/second.component';
-import { ThirdComponent } from './components/third/third.component';
+import { ThirdComponent } from './components/third/third.compnent';
 
 @NgModule({
   declarations: [AppComponent, SecondComponent, ThirdComponent],
