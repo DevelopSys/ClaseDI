89fbf8b8d2dc8331a18b1cec32795deeda30b1f8
Develop and System <developandsystem@gmail.com>
2022-02-15 12:42:00 +0100

    uipdate

--- T3-Angular/T2/codigos2021/proyecto_noticias/src/app/pipes/imagenes.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImagenesPipe } from './imagenes.pipe';
-
-describe('ImagenesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ImagenesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
