3b84e28b128e4b9faf84f510b67700cd93cf57ca
Develop and System <developandsystem@gmail.com>
2022-10-17 20:46:30 +0200

    update

---
+++ T2-Java/T4/codigos2223/Penstanias_Ret/src/main/java/com/example/penstanias_ret/MainController.java
@@ -0,0 +1,14 @@
+package com.example.penstanias_ret;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
