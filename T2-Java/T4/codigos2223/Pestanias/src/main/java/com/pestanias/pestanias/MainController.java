1215303a2f3e22ac7cd2857a94f2ab002f63eb37
Develop and System <developandsystem@gmail.com>
2022-10-17 13:44:29 +0200

    update

--- T2-Java/T4/codigos2223/Pestanias/src/main/java/com/pestanias/pestanias/MainController.java
+++ T2-Java/T4/codigos2223/Pestanias/src/main/java/com/pestanias/pestanias/MainController.java
@@ -8,12 +8,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.kordamp.ikonli.javafx.FontIcon;
 
@@ -34,8 +38,19 @@ public class MainController implements Initializable {
     @FXML
     private Label labelNombre, labelDesc, labelComision;
     @FXML
-    TextField textField;
+    private TextField textFiledUno, textFiledDos;
+    @FXML
+    private Button botonSuma, botonResta, botonDiv, botonMulti, botonIgual, botonMostrar, botonOcultar;
 
+    @FXML
+    private GridPane gridBotones;
+    @FXML
+    private VBox panelMostrar;
+
+    @FXML
+    private BorderPane panelGeneral;
+
+    private int tipoOperacion =-1;
     private DropShadow sombraExterior;
     private ToggleGroup grupoRadios;
 
@@ -54,9 +69,9 @@ public class MainController implements Initializable {
 
     private void asociarDatos() {
 
-        radio1.setUserData(new TipoPago("Tarjeta","PAgo con trajeta bancaria",0));
-        radio2.setUserData(new TipoPago("Transferencia","PAgo con transferecia bacanria",10));
-        radio3.setUserData(new TipoPago("PayPal","PAgo con aplicacion paypal",20));
+        radio1.setUserData(new TipoPago("Tarjeta", "PAgo con trajeta bancaria", 0));
+        radio2.setUserData(new TipoPago("Transferencia", "PAgo con transferecia bacanria", 10));
+        radio3.setUserData(new TipoPago("PayPal", "PAgo con aplicacion paypal", 20));
     }
 
     private void configurarBotones() {
@@ -68,7 +83,7 @@ public class MainController implements Initializable {
         botonNormalDos.setBackground(null);
 
         botonToggle.setBackground(null);
-        if (botonToggle.isSelected()){
+        if (botonToggle.isSelected()) {
             botonToggle.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("switchon.png"))));
         } else {
             botonToggle.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("switchoff.png"))));
@@ -79,12 +94,26 @@ public class MainController implements Initializable {
     private void instancias() {
         sombraExterior = new DropShadow();
         grupoRadios = new ToggleGroup();
-        grupoRadios.getToggles().addAll(radio1,radio2,radio3);
+        grupoRadios.getToggles().addAll(radio1, radio2, radio3);
     }
 
     private void acciones() {
         botonNormal.setOnAction(new ManejoPulsaciones());
         botonNormalDos.setOnAction(new ManejoPulsaciones());
+        botonMostrar.setOnAction(new ManejoPulsaciones());
+        botonOcultar.setOnAction(new ManejoPulsaciones());
+        for (Node child : gridBotones.getChildren()) {
+            if (child instanceof Button){
+                ((Button)child).setOnAction(new ManejoPulsaciones());
+            }
+        }
+
+        /*botonSuma.setOnAction(new ManejoPulsaciones());
+        botonResta.setOnAction(new ManejoPulsaciones());
+        botonMulti.setOnAction(new ManejoPulsaciones());
+        botonDiv.setOnAction(new ManejoPulsaciones());
+        botonIgual.setOnAction(new ManejoPulsaciones());*/
+
         // radio1.setOnAction(new ManejoPulsaciones());
         // botonToggle.setOnAction(new ManejoPulsaciones());
         grupoRadios.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
@@ -111,14 +140,14 @@ public class MainController implements Initializable {
                                 Boolean oldValue, Boolean newValue) {
                 botonNormal.setDisable(newValue);
                 botonNormalDos.setDisable(newValue);
-                if (newValue){
+                if (newValue) {
                     botonToggle.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("switchon.png"))));
-                }else {
+                } else {
                     botonToggle.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("switchoff.png"))));
                 }
             }
         });
-        textField.setOnKeyTyped(new EventHandler<KeyEvent>() {
+        textFiledUno.setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
                 char letraPulsada = keyEvent.getCharacter().charAt(0);
@@ -129,61 +158,107 @@ public class MainController implements Initializable {
         //botonNormal.addEventHandler(MouseEvent.MOUSE_RELEASED, new ManejoRaton());
     }
 
-    class ManejoRaton implements EventHandler<MouseEvent>{
+    class ManejoRaton implements EventHandler<MouseEvent> {
         @Override
         public void handle(MouseEvent mouseEvent) {
 
-            if (mouseEvent.getEventType() == MouseEvent.MOUSE_ENTERED){
-                if (mouseEvent.getSource() == botonNormal){
+            if (mouseEvent.getEventType() == MouseEvent.MOUSE_ENTERED) {
+                if (mouseEvent.getSource() == botonNormal) {
                     botonNormal.setEffect(sombraExterior);
-                } else if (mouseEvent.getSource() == botonNormalDos){
+                } else if (mouseEvent.getSource() == botonNormalDos) {
                     botonNormalDos.setEffect(sombraExterior);
                 }
-            }
-            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED){
-                if (mouseEvent.getSource() == botonNormal){
+            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED) {
+                if (mouseEvent.getSource() == botonNormal) {
                     botonNormal.setEffect(null);
-                } else if (mouseEvent.getSource() == botonNormalDos){
+                } else if (mouseEvent.getSource() == botonNormalDos) {
                     botonNormalDos.setEffect(null);
                 }
-            }
-            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED){
+            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED) {
                 //System.out.println("Raton pressed");
                 botonNormal.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("power_off.png"))));
-            }
-            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED){
+            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED) {
                 System.out.println("Raton clicked");
                 botonNormal.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("power_on.png"))));
-            }
-            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_RELEASED){
+            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_RELEASED) {
                 System.out.println("Raton released");
             }
         }
     }
 
-    class ManejoPulsaciones implements EventHandler<ActionEvent>{
+    class ManejoPulsaciones implements EventHandler<ActionEvent> {
+
+
 
         @Override
         public void handle(ActionEvent actionEvent) {
             //System.out.println("Boton pulsado");
-            if (actionEvent.getSource() == botonNormal){
+            if (actionEvent.getSource() == botonNormal) {
                 botonToggle.setSelected(true);
-                System.out.println(textField.getText());
+                System.out.println(textFiledUno.getText());
             }
-            else if(actionEvent.getSource() == botonNormalDos){
-                RadioButton radioSeleccionado  = (RadioButton) grupoRadios.getSelectedToggle();
+            else if (actionEvent.getSource() == botonNormalDos) {
+                RadioButton radioSeleccionado = (RadioButton) grupoRadios.getSelectedToggle();
                 TipoPago tipoPago = (TipoPago) radioSeleccionado.getUserData();
                 System.out.println(tipoPago.getComision());
                 System.out.println(tipoPago.getNombre());
                 System.out.println(tipoPago.getDescripcion());
+            }
+            else if (actionEvent.getSource() == botonSuma){
+                tipoOperacion = 0;
+                /*if (Character.isDigit(textFiledUno.getText().charAt(0)) && Character.isDigit(textFiledDos.getText().charAt(0))){
+                    int suma = Integer.parseInt(String.valueOf(textFiledUno.getText().charAt(0))) + Integer.parseInt(String.valueOf(textFiledDos.getText().charAt(0)));
+                    System.out.println("La suma de los valores es "+suma);
+                } else {
+                    System.out.println("Alguno de los elementos no es numero");
+                }*/
+
+            }
+            else if (actionEvent.getSource() == botonResta){
+                tipoOperacion = 1;
+            }
+            else if (actionEvent.getSource() == botonMulti){
+                tipoOperacion = 2;
+            }
+            else if (actionEvent.getSource() == botonDiv){
+                tipoOperacion = 3;
+            }
+            else if (actionEvent.getSource() == botonIgual){
+                int op1 = Integer.parseInt(String.valueOf(textFiledUno.getText().charAt(0)));
+                int op2 = Integer.parseInt(String.valueOf(textFiledDos.getText().charAt(0)));
+                double resultado = 0.0;
+                switch (tipoOperacion){
+                    case 0:
+                        resultado = op1+op2;
+                        break;
+                    case 1:
+                        resultado = op1-op2;
+                        break;
+                    case 2:
+                        resultado = op1*op2;
+                        break;
+                    case 3:
+                        resultado = (double) op1/ op2;
+                        break;
+                }
+                System.out.printf("El resutaldo de la operacion es %.2f",resultado);
+            }
+            else if (actionEvent.getSource() == botonMostrar){
+
+                //panelMostrar.setVisible(true);
+                panelGeneral.setRight(panelMostrar);
+            } else if(actionEvent.getSource() == botonOcultar){
+                // VBOX --
+                //panelMostrar.setVisible(false);
+                panelGeneral.getChildren().remove(panelMostrar);
             }
         }
     }
 }
 
 /*
-* 1- CAMBIAR LA IMAGEN DEL TOGGLE PARA QUE PONGA EL ON Y EL OFF
-* 2- AL CAMBIAR LA SELECCIÓN DEL TOGGLE GROUP APARECE LA INFORMACION EN 3 LABEL
-* 3- AL PULSAR EL BOTON DE OFF APARECE POR CONSOLA LA INFORMACION DEL
-* TOGGLE SELECCIONADO
-* */
+ * 1- CAMBIAR LA IMAGEN DEL TOGGLE PARA QUE PONGA EL ON Y EL OFF
+ * 2- AL CAMBIAR LA SELECCIÓN DEL TOGGLE GROUP APARECE LA INFORMACION EN 3 LABEL
+ * 3- AL PULSAR EL BOTON DE OFF APARECE POR CONSOLA LA INFORMACION DEL
+ * TOGGLE SELECCIONADO
+ * */
