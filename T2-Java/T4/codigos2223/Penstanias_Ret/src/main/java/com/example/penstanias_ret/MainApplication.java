888b2b432f58238bef33f75d6bbe35b86af7c6e6
Develop and System <developandsystem@gmail.com>
2022-10-19 15:59:22 +0200

    update

--- T2-Java/T4/codigos2223/Penstanias_Ret/src/main/java/com/example/penstanias_ret/MainApplication.java
+++ T2-Java/T4/codigos2223/Penstanias_Ret/src/main/java/com/example/penstanias_ret/MainApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Pestañias");
         stage.setScene(scene);
--- T2-Java/T4/codigos2223/Penstanias_Ret/src/main/java/com/example/penstanias_ret/MainController.java
+++ T2-Java/T4/codigos2223/Penstanias_Ret/src/main/java/com/example/penstanias_ret/MainController.java
@@ -1,14 +1,67 @@
 package com.example.penstanias_ret;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
+    @FXML
+    private Button botonNormal, botonNormalDos;
+
+    @FXML
+    private ToggleButton toggle;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        instancias();
+        acciones();
+
+    }
+
+    private void acciones() {
+        // acciones
+            // directa setOnEvento setOnAction --> EventHander<ActionEvent>
+
+        botonNormal.setOnAction(new ManejoPulsaciones());
+        botonNormalDos.setOnAction(new ManejoPulsaciones());
+        botonNormal.addEventHandler(MouseEvent.MOUSE_ENTERED,new ManejoRaton());
+        botonNormal.addEventHandler(MouseEvent.MOUSE_EXITED,new ManejoRaton());
+    }
+
+    private void instancias() {
+
+    }
+
+
+    class ManejoRaton implements EventHandler<MouseEvent>{
+
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            //System.out.println("Evento raton generado");
+            if (mouseEvent.getEventType() == MouseEvent.MOUSE_ENTERED){
+                System.out.println("Evento raton entered");
+                System.out.println(mouseEvent.getSceneX());
+                System.out.println(mouseEvent.getSceneY());
+            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED){
+                System.out.println("Evento raton exited");
+                System.out.println(mouseEvent.getSceneX());
+                System.out.println(mouseEvent.getSceneY());
+            }
+        }
+    }
+    class ManejoPulsaciones implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            System.out.println("Boton pulsado");
+        }
     }
 }
