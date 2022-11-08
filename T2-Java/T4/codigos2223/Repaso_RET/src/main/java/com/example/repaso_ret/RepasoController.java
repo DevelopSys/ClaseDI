e0ee6614f3e6fad001e60dfdfe5bb723698547f7
Develop and System <developandsystem@gmail.com>
2022-11-08 16:46:48 +0100

    update

---
+++ T2-Java/T4/codigos2223/Repaso_RET/src/main/java/com/example/repaso_ret/RepasoController.java
@@ -0,0 +1,96 @@
+package com.example.repaso_ret;
+
+import com.example.repaso_ret.model.Equipo;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RepasoController implements Initializable {
+
+
+    @FXML
+    private Label etiquetaEquipo;
+
+    @FXML
+    private ImageView imagen;
+
+    @FXML
+    private RadioButton radioRayo, radioAtleti, radioMadrid;
+
+    @FXML private BorderPane borderGeneral;
+    @FXML private GridPane gridLateral;
+
+    @FXML private ToggleButton toggleMostar;
+
+    private ToggleGroup grupoEquipos;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        instancias();
+        asociarDatos();
+        acciones();
+        initGUI();
+        //botonInferior.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("francia.jpg"))));
+        //imagen.setImage(new Image("http://as01.epimg.net/img/comunes/fotos/fichas/equipos/large/2.png"));
+    }
+
+    private void initGUI() {
+        toggleMostar.setSelected(false);
+        borderGeneral.getChildren().remove(gridLateral);
+    }
+
+    private void acciones() {
+
+
+        toggleMostar.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                if (t1){
+                    borderGeneral.setLeft(gridLateral);
+                } else  {
+                    borderGeneral.getChildren().remove(gridLateral);
+                }
+            }
+        });
+        grupoEquipos.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                Equipo seleccionado = (Equipo) ((RadioButton)t1).getUserData();
+                imagen.setImage(new Image(seleccionado.getImage()));
+                //etiquetaEquipo.setText(seleccionado.getCampo());
+                if (seleccionado.getNombre().equalsIgnoreCase("real madrid")){
+                    toggleMostar.setSelected(true);
+                } else {
+                    toggleMostar.setSelected(false);
+                }
+            }
+        });
+    }
+
+    private void asociarDatos() {
+        radioRayo.setUserData(new Equipo("Rayo Vallecano",
+                "http://as01.epimg.net/img/comunes/fotos/fichas/equipos/large/2.png",
+                "Vallecas"));
+        radioMadrid.setUserData(new Equipo("Real Madrid",
+                "http://as01.epimg.net/img/comunes/fotos/fichas/equipos/large/1.png",
+                "Bernabeu"));
+        radioAtleti.setUserData(new Equipo("Atletico de Madrid",
+                "https://i.pinimg.com/736x/34/d8/55/34d855a5e063ea145a4804b2d1c24e7d--madrid-soccer.jpg",
+                "Metropolitano"));
+    }
+
+    private void instancias() {
+        grupoEquipos = new ToggleGroup();
+        grupoEquipos.getToggles().addAll(radioRayo,radioMadrid,radioAtleti);
+    }
+}
---
+++ T2-Java/T4/codigos2223/Repaso_RET/src/main/java/com/example/repaso_ret/model/Equipo.java
@@ -0,0 +1,36 @@
+package com.example.repaso_ret.model;
+
+public class Equipo {
+
+    private String nombre, image, campo;
+
+    public Equipo(String nombre, String image, String campo) {
+        this.nombre = nombre;
+        this.image = image;
+        this.campo = campo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+}
