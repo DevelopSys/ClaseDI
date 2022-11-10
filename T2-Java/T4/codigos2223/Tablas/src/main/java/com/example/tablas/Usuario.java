92716b182c621737fdba943811ba458753624dfe
Develop and System <developandsystem@gmail.com>
2022-11-10 10:49:16 +0100

    update

---
+++ T2-Java/T4/codigos2223/Tablas/src/main/java/com/example/tablas/Usuario.java
@@ -0,0 +1,64 @@
+package com.example.tablas;
+
+public class Usuario {
+
+    private String nombre, apellido, dni, correo;
+    private int telefono, id;
+
+    public Usuario(String nombre, String apellido, String dni, String correo, int telefono, int id) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
