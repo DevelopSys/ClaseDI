db3cd9971974d5bce4fc87cf3dc52a27bc3f3859
Develop and System <developandsystem@gmail.com>
2022-12-15 12:43:40 +0100

    update

---
+++ T3-Angular/T6/codigos2223/inicio/js/utilizacion.js
@@ -0,0 +1,23 @@
+let ejemploObjeto = new ClaseEjemplo("p1", "p2", ["p3"]);
+ejemploObjeto.mostrarDatos();
+
+// una clase que represente una nota
+/* una nota tiene un titulo,
+ tiene una fecha, 
+ tiene una descripcion,
+tiene un completada ?, 
+conjunto de tareas
+  
+  set y get
+  agregar una tarea dentro de la nota
+  muestre todas las tareas pendientes de la nota
+muestre todas las tareas completas de la nota
+  
+  */
+
+// realizar practica de di
+// practica di
+// 22/12/2022 --> DATE
+// false
+// realizacion de una clase js para hacer una app de notas
+// [ ("hacer una clase",boolean), ("hacer unos metodos",boolean)]
