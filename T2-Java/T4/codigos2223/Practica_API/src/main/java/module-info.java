31322e2c0a9557c755399e3577b4c912c5ffa978
Martinezai783 <113194543+Martinezai783@users.noreply.github.com>
2022-12-15 09:57:33 +0100

    Update Repaso

---
+++ T2-Java/T4/codigos2223/Practica_API/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.practica_api {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.practica_api to javafx.fxml;
+    exports com.example.practica_api;
+}
