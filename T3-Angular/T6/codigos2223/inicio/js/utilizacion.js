bedb3a33668af1ceae3ea8a47abeb5e7ac07f40e
Develop and System <developandsystem@gmail.com>
2022-12-20 13:17:22 +0100

    Update utilizacion.js

--- T3-Angular/T6/codigos2223/inicio/js/utilizacion.js
+++ T3-Angular/T6/codigos2223/inicio/js/utilizacion.js
@@ -1,27 +1,6 @@
 let ejemploObjeto = new ClaseEjemplo("p1", "p2", ["p3"]);
 ejemploObjeto.mostrarDatos();
 
-// una clase que represente una nota
-/* una nota tiene un titulo,
- tiene una fecha, 
- tiene una descripcion,
-tiene un completada ?, 
-conjunto de tareas
-  
-  set y get
-  agregar una tarea dentro de la nota
-  muestre todas las tareas pendientes de la nota
-muestre todas las tareas completas de la nota
-  
-  */
-
-// nota: realizar practica de di
-// titulo: practica di
-// fecha: 22/12/2022 --> DATE
-// completada: false
-// descripcion: realizacion de una clase js para hacer una app de notas
-// tareas: [ ("hacer una clase",boolean), ("hacer unos metodos",boolean)]
-
 class Tarea {
   titulo;
   completa;
