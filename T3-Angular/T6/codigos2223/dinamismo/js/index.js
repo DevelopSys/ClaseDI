ee16a1f91fcfe2680d4b0402fd5b7cf9f3943320
Develop and System <developandsystem@gmail.com>
2023-01-09 09:58:35 +0100

    update

---
+++ T3-Angular/T6/codigos2223/dinamismo/js/index.js
@@ -0,0 +1,24 @@
+// trabajar con etiquetas
+// trabajar con class
+// trabajar con id
+
+/* // elemento que tenga el id --> unico
+let elementoPorID = document.getElementById("lista");
+console.log(elementoPorID);
+// elemento que tenga el etiqueta --> lista (HTML ELEMENT lista)
+let elementoPorTag = document.getElementsByTagName("li");
+console.log(elementoPorTag);
+// elemento que tenga el class --> lista (HTML ELEMENT lista)
+let elementoPorClass = document.getElementsByClassName("m-4");
+console.log(elementoPorClass); */
+
+// #id .class tag
+// --> elemento único --> primer elemento --> Node
+//let elementQuery = document.querySelector("#lista");
+
+// --> elemento multiple --> todos los elementos --> NodeList
+//document.querySelectorAll();
+//let elementosQuery = document.querySelectorAll("#lista1 li");
+//let elementosQuery = document.querySelectorAll("ul.clase1");
+//let elementosQuery = document.querySelectorAll("#lista1.clase1");
+//console.log(elementosQuery[0]);
