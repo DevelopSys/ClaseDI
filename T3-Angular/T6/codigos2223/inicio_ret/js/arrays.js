ac5021b6418346ac3fea705c8c56268d0718f897
Develop and System <developandsystem@gmail.com>
2023-01-17 16:46:00 +0100

    update

--- T3-Angular/T6/codigos2223/inicio_ret/js/arrays.js
+++ T3-Angular/T6/codigos2223/inicio_ret/js/arrays.js
@@ -1,31 +1,31 @@
 let elementosAny = [];
-const elementosPalabra = ["Palabra 1", "Palabra 2"];
+const elementosPalabra = ["Palabra 1", "Palabra 2", "Palabra 3", "Palabra 4"];
 
 // obtener elementos
-console.log(elementosPalabra[1]);
-console.log(elementosPalabra[5]);
+/* console.log(elementosPalabra[1]);
+console.log(elementosPalabra[5]); */
 
 // cambiar
 
-elementosPalabra[0] = "Palabra 1 modificada";
+/* elementosPalabra[0] = "Palabra 1 modificada";
 elementosPalabra[2] = "Palabra 3";
-elementosPalabra[10] = "Palabra 10";
+elementosPalabra[10] = "Palabra 10"; */
 // ["Palabra 1", "Palabra 2", "Palabra 3"]
 
-for (const key in elementosPalabra) {
-  console.log(key);
-}
+//
 
-elementosPalabra.length = 100;
-console.log(elementosPalabra.length);
-
-elementosPalabra.forEach((element) => {
-  console.log(element);
-});
 /* elementosPalabra.forEach((element) => {
   console.log(element);
 }); */
 
 // añadir
+elementosPalabra.push("Palabra nueva"); // -> 5
+console.log(elementosPalabra.unshift("Palabra Inicio")); // -> 6
 
 // eliminar
+console.log(elementosPalabra.pop()); // palabra nueva
+console.log(elementosPalabra.shift()); // Palabra inicial
+
+elementosPalabra.forEach((element, index) => {
+  console.log(`${index} ${element}`);
+});
