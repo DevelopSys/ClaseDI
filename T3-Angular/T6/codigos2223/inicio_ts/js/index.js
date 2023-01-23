711ab3f56f953797b3c4b32a1f0beb818b926232
Develop and System <developandsystem@gmail.com>
2023-01-23 10:12:08 +0100

    update

---
+++ T3-Angular/T6/codigos2223/inicio_ts/js/index.js
@@ -0,0 +1,35 @@
+console.log("Declaracion de variables");
+// let var const
+var profesor1 = {
+  nombre: "Borja",
+  apellido: "Martin",
+  asignaturas: [
+    { nombre: "Programacion", horas: 8 },
+    { nombre: "Desarrollo interfaces", horas: 6 },
+  ],
+};
+var profesor2 = {
+  nombre: "Jesus",
+  apellido: "Niño",
+  asignaturas: [
+    { nombre: "Programacion procesos", horas: 4 },
+    { nombre: "Entornos de desarrollo", horas: 3 },
+  ],
+};
+
+// HACER UNA FUNCION QUE PIDA POR PARAMETROS UN ARRAY DE CUALQUIER (Any) COSA
+// Y MUESTRE POR CONSOLA EL NOMRBE DE LAS ASIGNATURAS
+
+/* let nombre: String = "Borja";
+let edad: Number = 38;
+let asignaturas: Array<String> = [
+  "Programacion",
+  "Desarrollo interfaces",
+  "Programacion Multimedia",
+];
+
+asignaturas.forEach((element) => {
+  console.log(element);
+});
+*/
+console.log("Ejemplo de profesores");
