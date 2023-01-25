5ab82fa914b2e891c494712510f029b249299c38
Develop and System <developandsystem@gmail.com>
2023-01-25 17:23:37 +0100

    update

--- T3-Angular/T6/codigos2223/dom_ret/js/index.js
+++ T3-Angular/T6/codigos2223/dom_ret/js/index.js
@@ -18,15 +18,35 @@ let listaUno = document.querySelector("#lista_uno");
 let listaDos = document.querySelector("#lista_dos");
 let select = document.querySelector("#select_lista");
 let elementosLi = document.querySelectorAll("ul.elemento li");
+let inputAgregar = document.querySelector("#input_agregar");
+let listaSeleccionada = -1;
 
-select.addEventListener("change", () => {
-  console.log(select.value);
+select.addEventListener("change", (e) => {
+  //console.log(select.value);
+  console.log(e.target.value);
+  //listaSeleccionada = select.value;
 });
+
+function agregarDato(dato, lista) {
+  let nodoLI = document.createElement("li");
+  nodoLI.className = "list-group-item";
+  nodoLI.textContent = dato;
+  lista.append(nodoLI);
+  //lista.innerHTML += `<li class="list-group-item">${dato}</li>`;
+}
+
 botonAgregar.addEventListener("click", (event) => {
+  if (inputAgregar.value) {
+    select.value == 1
+      ? agregarDato(inputAgregar.value, listaUno)
+      : agregarDato(inputAgregar.value, listaDos);
+  } else {
+    alert("No se han introducido datos");
+  }
   //console.log("boton puslado");
   //<li>Elemento nuevo</li>
   //listaUno.innerHTML += `<li class="list-group-item">Elemento prueba</li>`;
-  let nodoLI = document.createElement("li");
+  /* let nodoLI = document.createElement("li");
   nodoLI.textContent = "Elemento prueba";
   //nodoLI.className = "list-group-item";
   nodoLI.classList.add("list-group-item");
@@ -36,7 +56,7 @@ botonAgregar.addEventListener("click", (event) => {
   listaUno.append(nodoLI);
 });
 botonBorrar.addEventListener("click", (event) => {
-  console.log("boton puslado");
+  console.log("boton puslado"); */
 });
 
 /* elementosLi.forEach((element) => {
