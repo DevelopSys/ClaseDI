c2ba18ea6d8db3968e1b88ecf94107e5285a8f53
Develop and System <developandsystem@gmail.com>
2023-01-27 19:22:15 +0100

    update

--- T3-Angular/T6/codigos2223/dom_ret/js/index.js
+++ T3-Angular/T6/codigos2223/dom_ret/js/index.js
@@ -19,15 +19,181 @@ let listaDos = document.querySelector("#lista_dos");
 let select = document.querySelector("#select_lista");
 let elementosLi = document.querySelectorAll("ul.elemento li");
 let inputAgregar = document.querySelector("#input_agregar");
+let filaProfesores = document.querySelector(".row");
 let listaSeleccionada = -1;
-let profosores = [
+let profesores = [
   {
-    nombre: "",
-    apellido: "",
-    correo: "",
-    asignaturas: [{ nombre: "", horas: 4, siglas: "", ciclo: "", curso: "" }],
+    nombre: "Ejemplo",
+    apellido: "Ejemplo",
+    edad: 18,
+    correo: "ejemplo@gmail.om",
+    asignaturas: [
+      {
+        nombre: "Desarrollo de interfaces",
+        horas: 6,
+        siglas: "DI",
+        ciclo: "DAM",
+        curso: 2,
+      },
+      {
+        nombre: "Programacion multimedia",
+        horas: 6,
+        siglas: "PMDM",
+        ciclo: "DAM",
+        curso: 2,
+      },
+    ],
+    mostrarDatos: function () {
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.edad);
+    },
+  },
+  {
+    nombre: "Borja",
+    apellido: "Martin",
+    edad: 18,
+    correo: "bmartinh@gmail.om",
+    asignaturas: [
+      {
+        nombre: "Desarrollo de interfaces",
+        horas: 6,
+        siglas: "DI",
+        ciclo: "DAM",
+        curso: 2,
+      },
+      {
+        nombre: "Programacion multimedia",
+        horas: 6,
+        siglas: "PMDM",
+        ciclo: "DAM",
+        curso: 2,
+      },
+    ],
+    mostrarDatos: function () {
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.edad);
+    },
+  },
+  {
+    nombre: "Alberto",
+    apellido: "Fernandez",
+    edad: 18,
+    correo: "alberto.fernandez@gmail.om",
+    asignaturas: [
+      {
+        nombre: "Acceso a datos",
+        horas: 5,
+        siglas: "AD",
+        ciclo: "DAM",
+        curso: 2,
+      },
+    ],
+    mostrarDatos: function () {
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.edad);
+    },
+  },
+  {
+    nombre: "Ferando",
+    apellido: "Sanchez",
+    edad: 24,
+    correo: "fernando@gmail.om",
+    asignaturas: [
+      {
+        nombre: "Programacion",
+        horas: 8,
+        siglas: "PRO",
+        ciclo: "DAM",
+        curso: 1,
+      },
+      {
+        nombre: "Base datos",
+        horas: 6,
+        siglas: "BD",
+        ciclo: "DAM",
+        curso: 1,
+      },
+    ],
+    mostrarDatos: function () {
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.edad);
+    },
+  },
+  {
+    nombre: "Carlos",
+    apellido: "Sanchez",
+    edad: 28,
+    correo: "carlos@gmail.om",
+    asignaturas: [
+      {
+        nombre: "Entornos de Desarrollo",
+        horas: 3,
+        siglas: "ED",
+        ciclo: "DAM",
+        curso: 1,
+      },
+      {
+        nombre: "Sistemas de Gestión empresarial",
+        horas: 3,
+        siglas: "SGE",
+        ciclo: "DAM",
+        curso: 2,
+      },
+    ],
+    mostrarDatos: function () {
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.edad);
+    },
+  },
+  {
+    nombre: "Manuel",
+    apellido: "PErez",
+    edad: 28,
+    correo: "manuel@gmail.om",
+    asignaturas: [
+      {
+        nombre: "Empresa e iniciativa emprendedora",
+        horas: 3,
+        siglas: "EIE",
+        ciclo: "DAM",
+        curso: 2,
+      },
+    ],
+    mostrarDatos: function () {
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.edad);
+    },
   },
 ];
+
+profesores.forEach((item) => {
+  /*   item.apellido
+  item.nombre
+  item.correo */
+  // crear una carta y colocalar en el row filaProfesores
+  filaProfesores.innerHTML += `<div class="col">
+            <div class="card" style="width: 18rem">
+              <div class="card-body">
+                <h5 class="card-title">${item.nombre}</h5>
+                <h6 class="card-subtitle">${item.apellido}</h6>
+                <p class="card-text">
+                  ${item.correo}
+                </p>
+              </div>
+            </div>
+          </div>`;
+
+  let nodoLista = document.createElement("ul");
+  nodoLista.innerHTML; // agregar li
+  // nodolista agrego a la carta
+  // foreach de las asignaturas item.asignaturas --> agregarlas en la ul
+});
 
 select.addEventListener("change", (e) => {
   //console.log(select.value);
