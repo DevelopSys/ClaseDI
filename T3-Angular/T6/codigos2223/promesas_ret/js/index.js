b27c64c5d72e0976df57da43437e77ef39b61036
Develop and System <developandsystem@gmail.com>
2023-02-01 17:30:12 +0100

    update

--- T3-Angular/T6/codigos2223/promesas_ret/js/index.js
+++ T3-Angular/T6/codigos2223/promesas_ret/js/index.js
@@ -44,15 +44,49 @@ fetch("https://dummyjson.com/products")
     console.log("Conexion incorrecta");
   });
 
+let urlProductos = "https://dummyjson.com/products/category/";
 let selectCategorias = document.querySelector("select");
+let filaCartas = document.querySelector(".row .row");
+
 selectCategorias.addEventListener("change", (e) => {
-  console.log(e.target.value);
+  consultarProductos(e.target.value);
 });
+
+function consultarProductos(categoria) {
+  filaCartas.innerHTML = "";
+  fetch(urlProductos + categoria)
+    .then((ok) => {
+      console.log(ok);
+      return ok.json();
+    })
+    .then((ok1) => {
+      ok1.products.forEach((element) => {
+        //console.log(element.title);
+        // pintar cada carta en el innerHTML de la row
+        filaCartas.innerHTML += `<div class="col">
+              <div class="card" style="width: 18rem">
+                <img src="${element.images[0]}" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">${element.title}</h5>
+                  <h6 class="card-title">${element.price}</h6>
+                  <p class="card-text">
+                   ${element.description}
+                  </p>
+                  <button class="btn btn-primary">Agregar a carrito</button>
+                </div>
+              </div>
+            </div>`;
+      });
+    })
+    .catch((err) => {
+      alert("Conexion incorrecta");
+    });
+}
 
 fetch("https://dummyjson.com/products/categories")
   .then((ok) => ok.json())
   .then((ok1) => {
     ok1.forEach((element) => {
-      selectCategorias.innerHTML += `<option>${element}<option>`;
+      selectCategorias.innerHTML += `<option>${element}</option>`;
     });
   });
--- T3-Angular/T6/codigos2223/proyecto_ts/index.js
+++ T3-Angular/T6/codigos2223/proyecto_ts/index.js
@@ -1,2 +1,11 @@
-//import { ejemplo } from "./js/funciones";
-console.log("Ejemplo proyecto web");
+// any --> Object
+//function funcionNormal(param1: Number, param2: Number): {};
+function optativo(p1, p2, p3) {
+    console.log(p1);
+    console.log(p2);
+    if (p3) {
+        console.log(p3);
+    }
+}
+optativo("primero", "segundo", 12);
+optativo("primero", "segundo");
