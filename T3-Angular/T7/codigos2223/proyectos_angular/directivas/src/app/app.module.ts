7714ef926e6d242b6ad74bf04c7507c25b9eb5d0
Develop and System <developandsystem@gmail.com>
2023-02-14 09:28:08 +0100

    update

--- T3-Angular/T7/codigos2223/proyectos_angular/directivas/src/app/app.module.ts
+++ T3-Angular/T7/codigos2223/proyectos_angular/directivas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { DirectivaifComponent } from './components/directivaif/directivaif.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    EventosComponent,
+    DirectivaifComponent
   ],
   imports: [
     BrowserModule,
---
+++ T3-Angular/T7/codigos2223/proyectos_angular/directivas/src/app/components/directivaif/directivaif.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'directivas-directivaif',
+  templateUrl: './directivaif.component.html',
+  styleUrls: ['./directivaif.component.css'],
+})
+export class DirectivaifComponent {
+  mostrarElemento = true;
+}
