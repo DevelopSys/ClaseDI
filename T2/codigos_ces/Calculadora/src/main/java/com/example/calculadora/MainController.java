628fcd1d10eafccc46954b93018678a97561662b
Develop and System <developandsystem@gmail.com>
2023-10-09 09:18:55 +0200

    update

--- T2/codigos_ces/Calculadora/src/main/java/com/example/calculadora/MainController.java
+++ T2/codigos_ces/Calculadora/src/main/java/com/example/calculadora/MainController.java
@@ -1,4 +1,33 @@
 package com.example.calculadora;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private Button boton1;
+
+    @FXML
+    private Button boton2;
+
+    @FXML
+    private Button botonIgual;
+
+    @FXML
+    private Button botonMas;
+
+    @FXML
+    private TextField texcoCalculadora;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        
+
+    }
 }
