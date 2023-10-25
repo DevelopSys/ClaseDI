c82255f262fbf4810a49be14b0a22310626d7a4e
Develop and System <developandsystem@gmail.com>
2023-10-25 18:26:25 +0200

    update

--- T3/codigos_ret/T3_Listas/src/main/java/com/example/t3_listas/MainController.java
+++ T3/codigos_ret/T3_Listas/src/main/java/com/example/t3_listas/MainController.java
@@ -1,18 +1,22 @@
 package com.example.t3_listas;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import org.controlsfx.control.spreadsheet.Grid;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+public class MainController implements Initializable, EventHandler<ActionEvent> {
 
     @FXML
     private CheckMenuItem menuActivar;
@@ -26,10 +30,42 @@ public class MainController implements Initializable {
     @FXML
     private MenuItem menuSalir;
 
+    @FXML
+    private GridPane parteCentral;
+
+    private ToggleGroup grupoHabilitar;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        instancias();
         personalizarMenu();
+        acciones();
+    }
+
+    private void acciones() {
+
+        menuSalir.setOnAction(this);
+        grupoHabilitar.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                if (t1 != null) {
+                    if (((RadioMenuItem) t1).getText().equalsIgnoreCase("habilitar")) {
+                        parteCentral.setDisable(false);
+                    } else if ((((RadioMenuItem) t1).getText().equalsIgnoreCase("deshabilitar"))) {
+                        parteCentral.setDisable(true);
+                    }
+                } else {
+                    grupoHabilitar.selectToggle(toggle);
+                }
+
+            }
+        });
+    }
+
+    private void instancias() {
+        grupoHabilitar = new ToggleGroup();
+        grupoHabilitar.getToggles().addAll(menuHabilitar, menuDeshabilitar);
     }
 
     private void personalizarMenu() {
@@ -37,5 +73,29 @@ public class MainController implements Initializable {
         menuActivar.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("activar.png"))));
         menuDeshabilitar.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("hab.png"))));
         menuHabilitar.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("des.png"))));
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if (menuActivar.isSelected()) {
+            // Construidos - Alert
+            // ALERTA - WARN - INFO - CONF - INPUT - CHOICE
+            Alert dialogoConfirmacion = new Alert(Alert.AlertType.CONFIRMATION);
+            dialogoConfirmacion.setTitle("¿Salir?");
+            dialogoConfirmacion.setHeaderText("¿Estas seguro que quieres salir?");
+            
+            Optional<ButtonType> respuesta = dialogoConfirmacion.showAndWait();
+            if (respuesta.get() == ButtonType.OK) {
+                System.exit(0);
+            } else if (respuesta.get() == ButtonType.CANCEL) {
+                Alert dialogoInfo = new Alert(Alert.AlertType.INFORMATION);
+                dialogoInfo.setTitle("¿Salir?");
+                dialogoInfo.setHeaderText("Continuaras en la aplicacion");
+                dialogoInfo.show();
+            }
+
+        } else {
+            System.exit(0);
+        }
     }
 }
