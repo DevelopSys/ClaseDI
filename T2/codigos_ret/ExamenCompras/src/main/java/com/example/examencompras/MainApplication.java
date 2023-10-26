a23f4d47d12304cb858c0465e042ff96aad38dde
Develop and System <developandsystem@gmail.com>
2023-10-26 18:19:20 +0200

    update

---
+++ T2/codigos_ret/ExamenCompras/src/main/java/com/example/examencompras/MainApplication.java
@@ -0,0 +1,27 @@
+package com.example.examencompras;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ T2/codigos_ret/ExamenCompras/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.examencompras {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.examencompras to javafx.fxml;
+    exports com.example.examencompras;
+}
