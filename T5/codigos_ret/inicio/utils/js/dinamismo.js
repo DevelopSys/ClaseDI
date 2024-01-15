af07673ea5fb0e9f1b1c855b41ae43b1b91a2e5a
Develop and System <developandsystem@gmail.com>
2024-01-15 18:29:21 +0100

    update

--- T5/codigos_ret/inicio/utils/js/dinamismo.js
+++ T5/codigos_ret/inicio/utils/js/dinamismo.js
@@ -1,20 +1,12 @@
 let botonPedir = document.querySelector("#boton-pedir");
 // class id tag
 let nodoPuntuacionJugador = document.querySelector("#puntuacion-j1");
+let nodoNombreJugador = document.querySelector("#nombre-j1");
+let nodoListaCartasJ1 = document.querySelector("div#cartas-j1");
 let barajaCompleta = [];
 
-iniciarBajara();
-botonPedir.addEventListener("click", () => {
-  let cartaSacada = barajaCompleta.pop();
-  // asignar el valor de la variable dentro del html del nodo
-  //nodoPuntuacionJugador.innerHTML = contadorJ1;
-  nodoPuntuacionJugador.innerText =
-    Number(nodoPuntuacionJugador.textContent) + 1;
-  console.log(cartaSacada);
-});
-
 function iniciarBajara() {
-  const palos = ["C", "D", "P", "R"];
+  const palos = ["C", "D", "P", "T"];
   for (let i = 0; i < palos.length; i++) {
     for (let j = 1; j < 14; j++) {
       barajaCompleta.push(`${j}${palos[i]}`);
@@ -22,3 +14,37 @@ function iniciarBajara() {
   }
   barajaCompleta = _.shuffle(barajaCompleta);
 }
+function configurarNombre() {
+  let nombreJugador = prompt("Indica tu nombre");
+  nodoNombreJugador.innerText = nombreJugador;
+}
+
+window.addEventListener("load", () => {
+  //configurarNombre();
+  iniciarBajara();
+});
+
+botonPedir.addEventListener("click", () => {
+  let cartaSacada = barajaCompleta.pop();
+  // 4C
+  // 13C
+
+  let valorCarta = cartaSacada.substring(0, cartaSacada.length - 1);
+  if (Number(valorCarta) > 9) {
+    valorCarta = 10;
+  }
+  nodoPuntuacionJugador.innerText =
+    Number(nodoPuntuacionJugador.textContent) + Number(valorCarta);
+
+  nodoListaCartasJ1.innerHTML += `<img src='./utils/images/${cartaSacada}.png' 
+  class='m-2 animate__animated animate__fadeInRight' style='width: 20%' >`;
+  //let nodo = document.createElement("img");
+  //nodoLI.className = "list-group-item";
+  //nodoLI.textContent = `${cartaSacada}`;
+  //nodo.src = `./utils/images/${cartaSacada}.png`;
+  //nodoListaCartasJ1.appendChild(nodo);
+});
+
+setTimeout(() => {
+  // ejecuta tras esperar
+}, 100);
