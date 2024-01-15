c053ec2ea6324ac54b44dae0969b9767ae7c4db7
Develop and System <developandsystem@gmail.com>
2024-01-15 16:59:08 +0100

    update

--- T5/codigos_ret/inicio/utils/js/dinamismo.js
+++ T5/codigos_ret/inicio/utils/js/dinamismo.js
@@ -1,4 +1,24 @@
 let botonPedir = document.querySelector("#boton-pedir");
+// class id tag
+let nodoPuntuacionJugador = document.querySelector("#puntuacion-j1");
+let barajaCompleta = [];
+
+iniciarBajara();
 botonPedir.addEventListener("click", () => {
-  console.log("Boton pulsado");
+  let cartaSacada = barajaCompleta.pop();
+  // asignar el valor de la variable dentro del html del nodo
+  //nodoPuntuacionJugador.innerHTML = contadorJ1;
+  nodoPuntuacionJugador.innerText =
+    Number(nodoPuntuacionJugador.textContent) + 1;
+  console.log(cartaSacada);
 });
+
+function iniciarBajara() {
+  const palos = ["C", "D", "P", "R"];
+  for (let i = 0; i < palos.length; i++) {
+    for (let j = 1; j < 14; j++) {
+      barajaCompleta.push(`${j}${palos[i]}`);
+    }
+  }
+  barajaCompleta = _.shuffle(barajaCompleta);
+}
