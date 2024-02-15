5664ae9f55538dd89747db797afa42c02becd9fc
Develop and System <developandsystem@gmail.com>
2024-02-15 18:27:46 +0100

    update

---
+++ T6/codigos_ret/angular/examen/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  titulo = 'NOTICIAS RETAMAR';
+  imagen =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr9GDNL_qkqCc7WqsAnF_XXgtUpE6w7XpU-ZGuzUFJcg&s';
+}
---
+++ T6/codigos_ret/angular/examen/src/app/model/noticia.ts
@@ -0,0 +1,11 @@
+export interface Noticia {
+  source: any;
+  category: string;
+  author: string;
+  title: string;
+  url: string;
+  urlToImage: string;
+  description: string;
+  publishedAt: string;
+  content: string;
+}
