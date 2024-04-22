9aa1eeacbe1425a7195d3936db16297078d666de
Develop and System <developandsystem@gmail.com>
2024-04-22 16:10:04 +0200

    update

---
+++ RECUPERACION/INICIO_01/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.inicio_01 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.inicio_01 to javafx.fxml;
+    exports org.example.inicio_01;
+}
