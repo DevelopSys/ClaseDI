0adf4362aec5cff6f2c71cc66f57ab28e72301e9
Develop and System <developandsystem@gmail.com>
2024-10-09 17:48:49 +0200

    update

---
+++ T2/codigos_ret/inicio/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.inicio {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.inicio to javafx.fxml;
+    exports org.example.inicio;
+}
