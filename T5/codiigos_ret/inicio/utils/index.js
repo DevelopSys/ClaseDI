8b4086463c43bc6f92596ff5a1cc9fd25f4c2080
Develop and System <developandsystem@gmail.com>
2025-01-09 18:37:48 +0100

    update

---
+++ T5/codiigos_ret/inicio/utils/index.js
@@ -0,0 +1,16 @@
+/* esto es el inicio del codigo JS */
+// variables
+let nombre = "Borja"; // nombre: string -> Object
+let edad = 40; // number
+let experiencia = false; // bool
+let conocimientos = []; // Object -> Arraylist
+let nulo = null; // null
+let cosa; // undefined
+// NaN -> not a number
+const DNI = "123123A";
+
+console.log("Este es el primer mensaje desde codigo JS");
+console.log("Esto es un segundo comentario");
+console.log("Mi nombre es " + nombre + " y tengo " + edad + " años");
+console.log(`Mi nombre es ${nombre} y tengo ${edad} años`);
+console.log(`Mi nombre es ${nombre}`);
