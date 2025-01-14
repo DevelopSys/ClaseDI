de22d5e8a421932013594bf28be5b31a05c10803
Develop and System <developandsystem@gmail.com>
2025-01-14 16:10:26 +0100

    update

---
+++ T5/codigos_ces/proyectos angular/tareas/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
--- T5/codiigos_ret/inicio/utils/index.js
+++ T5/codiigos_ret/inicio/utils/index.js
@@ -14,3 +14,41 @@ console.log("Esto es un segundo comentario");
 console.log("Mi nombre es " + nombre + " y tengo " + edad + " años");
 console.log(`Mi nombre es ${nombre} y tengo ${edad} años`);
 console.log(`Mi nombre es ${nombre}`);
+
+/* Swal.fire({
+  title: "Bienvenido",
+  text: `Esto es un mensaje producido por ${nombre}`,
+  icon: "success",
+}); */
+// alert -> muestra info
+// confirm -> yes/no
+// prompt -> text
+/* let resultado = confirm("¿Estas seguro que quieres continuar?");
+if (resultado) {
+  nombre = prompt("Por favor introduce tu nombre");
+  alert(`Bienvenido ${nombre}`);
+}
+console.log(resultado); */
+/* let numeroUno = Number(prompt("Por favor introduce primer numero"));
+let numeroDos = Number(prompt("Por favor introduce segundo numero"));
+let respuesta = confirm("¿Estas seguro que quieres continuar?");
+if (!isNaN(numeroUno) && !isNaN(numeroDos)) {
+  if (respuesta) {
+    let suma = numeroUno + numeroDos;
+    let resta = numeroUno - numeroDos;
+    let multi = numeroUno * numeroDos;
+    let div = numeroUno / numeroDos;
+    let mod = numeroUno % numeroDos;
+    alert(
+      `Los resultado de las operaciones son\nSuma:${suma}\nResta:${resta}\nMulti:${multi}\nDiv:${div}\nMod ${mod}`
+    );
+  } else {
+    alert("No has continuado, por lo que termino");
+  }
+} else {
+  alert("Cuidado, datos incorrectos");
+} */
+// pedir por prompt dos numeros
+// pedir confirmacion si quiero realizar todas
+// las operaciones
+// si -> mostrar en un alert todas las operaciones (+,-,*,/,%)
