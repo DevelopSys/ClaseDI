3c92962602b6139cdf73921d943ad602ae82a961
Develop and System <developandsystem@gmail.com>
2025-01-15 14:25:33 +0100

    update

--- T5/codigos_ces/proyectos angular/tareas/src/app/app.module.ts
+++ T5/codigos_ces/proyectos angular/tareas/src/app/app.module.ts
@@ -4,14 +4,15 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, FormularioComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [provideClientHydration(withEventReplay())],
   bootstrap: [AppComponent],
 })
--- T5/codigos_ces/proyectos angular/tareas/src/app/components/home/home.component.ts
+++ T5/codigos_ces/proyectos angular/tareas/src/app/components/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
 
-import { log } from 'node:console';
-
 @Component({
   selector: 'app-home',
   standalone: false,
@@ -14,20 +12,42 @@ export class HomeComponent {
   nombre: String = 'Borja';
   asignatura: String = 'PMDM';
   experiencia = false;
+  opcion = 'Seleccionar opcion';
   imagen =
     'https://angular.dev/assets/images/press-kit/angular_wordmark_gradient.png';
+
+  cambioSelect(event: Event) {
+    console.log((event.target as HTMLSelectElement).value);
+    //this.mostrarDatos((event.target as HTMLSelectElement).value);
+  }
+
+  mostrarDatos(dato: String) {}
 
   cambiarFondo() {
     this.imagen =
       'https://logowik.com/content/uploads/images/angular9826.logowik.com.webp';
   }
 
-  obtenerDatos(nombre: String, apellido: String, correo: String) {
-    Swal.fire({
-      title: 'Error!',
-      text: 'Do you want to continue',
-      icon: 'error',
-      confirmButtonText: 'Cool',
-    });
+  obtenerDatos(
+    nombre: String,
+    apellido: String,
+    correo: String,
+    opcion: String
+  ) {
+    if (nombre.length > 0 && apellido.length > 0 && correo.length > 0) {
+      Swal.fire({
+        title: 'Exito!',
+        text: 'Opcion: ' + opcion,
+        icon: 'success',
+        confirmButtonText: 'Cool',
+      });
+    } else {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Error al sacar los datos',
+        icon: 'error',
+        confirmButtonText: 'Cool',
+      });
+    }
   }
 }
