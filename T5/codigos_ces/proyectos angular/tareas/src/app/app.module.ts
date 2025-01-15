7bbce221c8faa2e06fd40681e6c47016b6ddfc7f
Develop and System <developandsystem@gmail.com>
2025-01-15 13:29:50 +0100

    update

--- T5/codigos_ces/proyectos angular/tareas/src/app/app.module.ts
+++ T5/codigos_ces/proyectos angular/tareas/src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- T5/codigos_ces/proyectos angular/tareas/src/app/components/home/home.component.ts
+++ T5/codigos_ces/proyectos angular/tareas/src/app/components/home/home.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  nombre: String = 'Borja';
+  asignatura: String = 'PMDM';
+  experiencia = false;
+  imagen =
+    'https://angular.dev/assets/images/press-kit/angular_wordmark_gradient.png';
 
+  cambiarFondo() {
+    this.imagen =
+      'https://logowik.com/content/uploads/images/angular9826.logowik.com.webp';
+  }
+
+  obtenerDatos(nombre: String, apellido: String, correo: String) {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool',
+    });
+  }
 }
