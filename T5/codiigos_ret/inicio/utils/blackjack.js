b2fb4e65676a64e5c6d51a7a0387a08698ba1cec
Develop and System <developandsystem@gmail.com>
2025-01-16 18:22:36 +0100

    update

--- T5/codiigos_ret/inicio/utils/blackjack.js
+++ T5/codiigos_ret/inicio/utils/blackjack.js
@@ -1,42 +1,93 @@
 let botonPedir = document.querySelector("#pedirCarta");
+const palos = ["C", "D", "T", "P"];
 // baraja -> 1C K CDPT 12345678910JQK
 let baraja = [];
-const palos = ["C", "D", "T", "P"];
-for (let i = 0; i < palos.length; i++) {
-  for (let j = 1; j <= 13; j++) {
-    if (j == 11) {
-      baraja.push(`J${palos[i]}`);
-    } else if (j == 12) {
-      baraja.push(`Q${palos[i]}`);
-    } else if (j == 13) {
-      baraja.push(`K${palos[i]}`);
-    } else {
-      baraja.push(`${j}${palos[i]}`);
+let puntuacion = 0;
+
+function iniciarBaraja() {
+  puntuacion = 0;
+  baraja = [];
+  for (let i = 0; i < palos.length; i++) {
+    for (let j = 1; j <= 13; j++) {
+      if (j == 11) {
+        baraja.push(`J${palos[i]}`);
+      } else if (j == 12) {
+        baraja.push(`Q${palos[i]}`);
+      } else if (j == 13) {
+        baraja.push(`K${palos[i]}`);
+      } else {
+        baraja.push(`${j}${palos[i]}`);
+      }
     }
+  }
+  baraja = _.shuffle(baraja);
+}
+
+function obtenerValorCarta(carta) {
+  let valor = carta.substring(0, carta.length - 1);
+  if (isNaN(parseInt(valor))) {
+    console.log(valor);
+    return 10;
+  }
+  return parseInt(valor);
+}
+
+function pedirCarta() {
+  let valor = obtenerValorCarta(baraja.pop());
+  puntuacion += valor;
+  if (puntuacion == 21) {
+    // ganado
+    Swal.fire({
+      title: "Resultado",
+      text: "Enhorabuena, has ganado el juego",
+      icon: "sucess",
+    });
+  } else if (puntuacion > 21) {
+    Swal.fire({
+      title: `Lo siento has persido con una puntuación de ${puntuacion}. Quieres volver a jugar?`,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "SI",
+      denyButtonText: `NO`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        iniciarBaraja();
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "Resultado",
+          text: `Gracias por participar`,
+          icon: "warning",
+        });
+        iniciarBaraja();
+      }
+    });
+  } else {
+    Swal.fire({
+      title: `Tu puntuacion es de ${puntuacion}, ¿Quieres sacar más cartas?`,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "SI",
+      denyButtonText: `NO`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        pedirCarta();
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "Resultado",
+          text: `Tu puntuación final es de ${puntuacion}}`,
+          icon: "warning",
+        });
+        iniciarBaraja();
+      }
+    });
   }
 }
 
-/* for (let index = 0; index < 50; index++) {
-  let n1 = parseInt(Math.random() * 50);
-  let n2 = parseInt(Math.random() * 50);
-  let temporal = baraja[n2];
-  baraja[n2] = baraja[n1];
-  baraja[n1] = temporal;
-} */
-baraja = _.shuffle(baraja);
+iniciarBaraja();
 
 botonPedir.addEventListener("click", (e) => {
-  // sacar por sweetAlert la carta que saca, indicando por separado!!!
-  // el palo y el valor de la carta
-  // 1,2,3,4,5,6,7,8,9,10
-  // J,!,K -> 10
-  // console.log("Boton pulsado correctamente");
-  let cartaSacada = baraja.pop();
-  let valor = cartaSacada.substring(0, cartaSacada.length - 1);
-  let palo = cartaSacada.substring(cartaSacada.length - 1);
-  console.log(`La carta sacada es ${cartaSacada}`);
-  console.log(`Su valor es ${valor}`);
-  console.log(`Su valor es ${palo}`);
+  pedirCarta();
 });
 
 /* let correo =  "borja@gmail.com"
