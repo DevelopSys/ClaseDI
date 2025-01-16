bc41f4e763c0efc95e2671f412e1da9fd0ee47f1
Develop and System <developandsystem@gmail.com>
2025-01-16 16:39:07 +0100

    update

--- T5/codiigos_ret/inicio/utils/blackjack.js
+++ T5/codiigos_ret/inicio/utils/blackjack.js
@@ -1,7 +1,30 @@
+let botonPedir = document.querySelector("#pedirCarta");
 // baraja -> 1C K CDPT 12345678910JQK
-"1C";
-"2C";
-"3C";
-"JC";
-"QC";
-"KC";
+let baraja = [];
+const palos = ["C", "D", "T", "P"];
+for (let i = 0; i < palos.length; i++) {
+  for (let j = 1; j <= 13; j++) {
+    if (j == 11) {
+      baraja.push(`J${palos[i]}`);
+    } else if (j == 12) {
+      baraja.push(`Q${palos[i]}`);
+    } else if (j == 13) {
+      baraja.push(`K${palos[i]}`);
+    } else {
+      baraja.push(`${j}${palos[i]}`);
+    }
+  }
+}
+
+/* for (let index = 0; index < 50; index++) {
+  let n1 = parseInt(Math.random() * 50);
+  let n2 = parseInt(Math.random() * 50);
+  let temporal = baraja[n2];
+  baraja[n2] = baraja[n1];
+  baraja[n1] = temporal;
+} */
+baraja = _.shuffle(baraja);
+
+botonPedir.addEventListener("click", (e) => {
+  console.log("Boton pulsado correctamente");
+});
