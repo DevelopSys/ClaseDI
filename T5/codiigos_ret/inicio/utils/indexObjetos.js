14e754d1647700f45a5fc73bf121fb031623a503
Develop and System <developandsystem@gmail.com>
2025-01-21 16:28:01 +0100

    update

--- T5/codiigos_ret/inicio/utils/indexObjetos.js
+++ T5/codiigos_ret/inicio/utils/indexObjetos.js
@@ -1,6 +1,17 @@
-let tarea = new Tarea("Tarea1", "Alta", false);
-let tareaOcio = new TareaOcio("Tarea2", "Media", false, "parque");
-let tareaTrabajo = new TareaTrabajo("Tarea3", "Baja", false, 40000);
+let tarea1 = new Tarea("Tarea1", "Alta", false);
+let tarea2 = new Tarea("Tarea1", "Alta", true);
+let tarea3 = new Tarea("Tarea1", "Alta", false);
+let tarea4 = new Tarea("Tarea1", "Alta", true);
+let tarea5 = new Tarea("Tarea1", "Alta", false);
+let tareaOcio1 = new TareaOcio("Tarea2", "Media", false, "parque");
+let tareaOcio2 = new TareaOcio("Tarea2", "Media", true, "parque");
+let tareaOcio3 = new TareaOcio("Tarea2", "Media", true, "parque");
+let tareaOcio4 = new TareaOcio("Tarea2", "Media", true, "parque");
+let tareaTrabajo1 = new TareaTrabajo("Tarea3", "Baja", true, 40000);
+let tareaTrabajo2 = new TareaTrabajo("Tarea3", "Baja", true, 40000);
+let tareaTrabajo3 = new TareaTrabajo("Tarea3", "Baja", true, 40000);
+let tareaTrabajo4 = new TareaTrabajo("Tarea3", "Baja", false, 40000);
+let tareaTrabajo5 = new TareaTrabajo("Tarea3", "Baja", true, 40000);
 let tareaJSON = {
   titulo: "Tarea 4",
   prioridad: "Media",
@@ -19,7 +30,42 @@ let tareaJSON = {
   },
 };
 
-tareaJSON.mostrarDatos();
+let tareas = [
+  tarea1,
+  tarea2,
+  tarea3,
+  tarea4,
+  tarea5,
+  tareaOcio1,
+  tareaOcio2,
+  tareaOcio3,
+  tareaOcio4,
+  tareaTrabajo1,
+  tareaTrabajo2,
+  tareaTrabajo3,
+  tareaTrabajo4,
+  tareaTrabajo5,
+  tareaJSON,
+];
+
+// lista
+tareas
+  .filter((element) => {
+    return element.completada;
+  })
+  .forEach((item) => {
+    console.log(item);
+  });
+
+/* tareas.forEach((element) => {
+  if (!element.completada) {
+    element.mostrarDatos();
+  }
+}); */
+
+// mostrar solo las completadas
+
+// tareaJSON.mostrarDatos();
 //tareaJSON.mostrarImportancia("media");
 /* JavaScriptObject Notatio
 par clave - valor
@@ -53,3 +99,10 @@ asdasdasdasdasdasd
 // tarea.setPrioridad = "Media";
 // console.log(tarea.getPrioridad);
 // tareaTrabajo.mostrarDatos();
+
+// baraja = [cartas]
+// Cartas -> new Carta(QC)
+// valor -> 10 -> calculado
+// palo -> C -> calculado
+// representacion QC
+// imagen QC.png -> calculado
