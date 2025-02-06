f01ab9283fb884e1186e076273cf23e9bca1630a
Develop and System <developandsystem@gmail.com>
2025-02-06 18:34:53 +0100

    update

--- T6/proyectos_angular/inicio/src/app/components/home/home.component.ts
+++ T6/proyectos_angular/inicio/src/app/components/home/home.component.ts
@@ -9,8 +9,24 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   bindingUno: string | undefined = 'primer binding';
   imagenUrl = 'https://i.ytimg.com/vi/Wjj8fluz6rk/maxresdefault.jpg';
+  nombre = '';
+  apellido = '';
+  correo = '';
+  profesion = '';
 
   cambiarImagen(url: string) {
     this.imagenUrl = url;
+  }
+
+  enviarDatos(
+    nombre: string,
+    apellido: string,
+    correo: string,
+    profesion: string
+  ) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.profesion = profesion;
   }
 }
