125c7e0a72a59cfad9ce23973887e27e71292744
Develop and System <developandsystem@gmail.com>
2025-09-10 17:30:18 +0200

    update

---
+++ Tema1/Inicial/src/Entrada.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Entrada {
+    /*
+    // variables
+    final String DNI = "123123AA";
+    String palabra = "asdasd";
+    char letra = 'A';
+    int nunmero = 123;
+    Integer numeroObj = 123;
+    // nunmero:Int = 123;
+    double altura = 12.23;
+    float base = 12.23f;
+    boolean exito = true;*/
+    // constructores
+    // metodos
+    // fun main(args:Array<String>){}
+    public static void main(String[] args) {
+
+        Scanner lecturaTeclado = new Scanner(System.in);
+        System.out.println("Por favor introduce tu nombre");
+        String nombre = lecturaTeclado.next();
+        System.out.println("Por favor introduce tu edad");
+        int edad = lecturaTeclado.nextInt();
+        System.out.println("Primer programa en Java");
+        System.out.println("Mi nombre es "+nombre);
+        System.out.printf("Mi nombre es %s y tengo %s%n"
+                ,nombre,edad);
+        System.out.println("Fin del mensaje");
+
+    }
+
+}
