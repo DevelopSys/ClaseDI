7c1f092f4e0a05032efa5e02020a0e82bcc7262d
Develop and System <developandsystem@gmail.com>
2025-10-29 10:40:29 +0100

    update

---
+++ Tema2/codigos_ces/FormularioAPP/src/main/java/org/example/formularioapp/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.formularioapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
