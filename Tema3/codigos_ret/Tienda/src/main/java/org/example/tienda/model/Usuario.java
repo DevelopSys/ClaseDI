e38713ed45482fc36d63c3e3255ea110a423ed5c
Develop and System <developandsystem@gmail.com>
2025-12-02 16:29:15 +0100

    Update Usuario.java

--- Tema3/codigos_ret/Tienda/src/main/java/org/example/tienda/model/Usuario.java
+++ Tema3/codigos_ret/Tienda/src/main/java/org/example/tienda/model/Usuario.java
@@ -1,4 +1,14 @@
 package org.example.tienda.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+    private int id;
+    private String nombre, correo, pass;
+    private int idPerfil;
 }
