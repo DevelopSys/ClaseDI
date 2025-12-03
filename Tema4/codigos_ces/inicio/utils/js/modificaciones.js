0430ecefa93cbda9b5f2689d0a4efbdcf6dc8e3c
Develop and System <developandsystem@gmail.com>
2025-12-03 11:38:31 +0100

    update

--- Tema4/codigos_ces/inicio/utils/js/modificaciones.js
+++ Tema4/codigos_ces/inicio/utils/js/modificaciones.js
@@ -1,5 +1,29 @@
 let botonAgregar = document.querySelector("#btnAgregar");
+let botonLimpiar = document.querySelector("#btnLimpiar");
+let inputNombre = document.querySelector("#inputNombre");
+let inputApellido = document.querySelector("#inputApellido");
+let inputFecha = document.querySelector("#inputFecha");
 botonAgregar.addEventListener("click", (ev) => {
-  console.log("Boton pulsado");
-  console.log(ev);
+  let nombre = inputNombre.value;
+  let apellido = inputApellido.value;
+  let fecha = inputFecha.value;
+
+  let cosa = "";
+  if (nombre.length == 0 || apellido.length == 0 || fecha.length == 0) {
+    alert("Los datos no estan completos");
+  } else {
+    console.log(`Nombre ${nombre}`);
+    console.log(`Apellido ${apellido}`);
+    console.log(`Fecha ${fecha}`);
+  }
 });
+
+botonLimpiar.addEventListener("click", (ev) => {
+  limpiarCampos(inputNombre, inputApellido, inputFecha);
+});
+
+function limpiarCampos() {
+  for (let index = 0; index < arguments.length; index++) {
+    arguments[index].value = "";
+  }
+}
