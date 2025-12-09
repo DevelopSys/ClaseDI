4b1768c6fac6e39ed076882dfecbeb9932ccc000
Develop and System <developandsystem@gmail.com>
2025-12-09 09:55:11 +0100

    update

--- Tema4/codigos_ces/inicio/utils/js/modificaciones.js
+++ Tema4/codigos_ces/inicio/utils/js/modificaciones.js
@@ -1,5 +1,6 @@
 let botonAgregar = document.querySelector("#btnAgregar");
 let botonLimpiar = document.querySelector("#btnLimpiar");
+let botonVaciar = document.querySelector("#btnVaciar");
 let inputNombre = document.querySelector("#inputNombre");
 let inputApellido = document.querySelector("#inputApellido");
 let inputFecha = document.querySelector("#inputFecha");
@@ -14,12 +15,37 @@ botonAgregar.addEventListener("click", (ev) => {
   if (nombre.length == 0 || apellido.length == 0 || fecha.length == 0) {
     lanzarDialogo("Error", "Faltan datos", "warning");
   } else {
-    agregarLi(nombre, apellido, fecha);
+    // agregarLi(nombre, apellido, fecha);
+    let nodoLI = crearNodo(
+      "li",
+      `${nombre} ${apellido} ${fecha}`,
+      "animate__animated animate__fadeInRight list-group-item d-flex justify-content-between align-items-start"
+    );
+    let nodoButton = crearNodo("button", `Eliminar`, "btn btn-danger", (e) => {
+      nodoLI.classList.remove("animate__fadeInRight");
+      nodoLI.classList.add("animate__wobble");
+      setTimeout(() => {
+        nodoLI.classList.remove("animate__wobble");
+        nodoLI.classList.add("animate__fadeOutLeft");
+        setTimeout(() => {
+          nodoLI.remove();
+        }, 600);
+      }, 600);
+    });
+
+    nodoLI.appendChild(nodoButton);
+
+    listaAgregados.appendChild(nodoLI);
   }
 });
 
 botonLimpiar.addEventListener("click", (ev) => {
   limpiarCampos(inputNombre, inputApellido, inputFecha);
+});
+
+botonVaciar.addEventListener("click", (e) => {
+  // listaAgregados.remove();
+  listaAgregados.innerHTML = "";
 });
 
 function limpiarCampos() {
@@ -36,12 +62,22 @@ function lanzarDialogo(title, text, icon) {
   });
 }
 
-function agregarLi(nombre, apellido, fecha) {
-  /* listaAgregados.innerHTML += `<li
+function crearNodo(tipo, texto, clases, funcion) {
+  let nodo = document.createAttribute(tipo);
+  nodo.innerText = texto;
+  if (funcion != undefined) {
+    nodo.addEventListener("click", funcion);
+  }
+  nodo.className = clases;
+  return nodo;
+}
+
+/* function agregarLi(nombre, apellido, fecha) {
+  listaAgregados.innerHTML += `<li
             class="animate__animated animate__fadeInRight list-group-item d-flex justify-content-between align-items-start"
           >
             ${nombre} ${apellido} ${fecha}
-          </li>`; */
+          </li>`;
 
   let nodoLI = document.createElement("li");
   let nodoBoton = document.createElement("button");
@@ -49,12 +85,20 @@ function agregarLi(nombre, apellido, fecha) {
   nodoBoton.classList.add("btn", "btn-danger");
   nodoBoton.innerText = "Borrar";
   nodoBoton.addEventListener("click", (e) => {
-    nodoLI.remove();
+    nodoLI.classList.remove("animate__fadeInRight");
+    nodoLI.classList.add("animate__wobble");
+    setTimeout(() => {
+      nodoLI.classList.remove("animate__wobble");
+      nodoLI.classList.add("animate__fadeOutLeft");
+      setTimeout(() => {
+        nodoLI.remove();
+      }, 600);
+    }, 600);
   });
 
   nodoLI.innerText = `${nombre} ${apellido} ${fecha}`;
-  /* nodoLI.className =
-    "animate__animated animate__fadeInRight list-group-item d-flex justify-content-between align-items-start"; */
+  nodoLI.className =
+    "animate__animated animate__fadeInRight list-group-item d-flex justify-content-between align-items-start";
   nodoLI.classList.add(
     "animate__animated",
     "animate__fadeInRight",
@@ -69,9 +113,9 @@ function agregarLi(nombre, apellido, fecha) {
   listaAgregados.appendChild(nodoLI);
   limpiarCampos(inputNombre, inputApellido, inputFecha);
 
-  /* lanzarDialogo(
+lanzarDialogo(
     "Agregado correctamente",
     `Usuario con nombre ${nombre} agregado correctamente`,
     "success"
-  ); */
-}
+  ); 
+} */
