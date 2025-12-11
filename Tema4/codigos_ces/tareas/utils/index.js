593cdda4cbc3f7c856b70d1c33c2a072d1cedd08
Develop and System <developandsystem@gmail.com>
2025-12-11 14:17:44 +0100

    update

--- Tema4/codigos_ces/tareas/utils/index.js
+++ Tema4/codigos_ces/tareas/utils/index.js
@@ -1,6 +1,33 @@
-let boton = document.querySelector("#btnAgregar");
-let parrafo = document.querySelector("#contenido");
-boton.addEventListener("click", () => {
-  let tarea1 = new tarea("Tarea 1", new Date(), "Baja");
-  tarea1.mostrarDatos();
+let botonAgregar = document.querySelector("#btnAgregar");
+let inputNombre = document.querySelector("#inputNombre");
+let inputFecha = document.querySelector("#inputFecha");
+let selectPrioridad = document.querySelector("#selectPrioridad");
+let lista = document.querySelector("#listaTareas");
+
+let tareas = [];
+
+botonAgregar.addEventListener("click", () => {
+  let nombre = inputNombre.value;
+  let fecha = inputFecha.value;
+  let prioridad = selectPrioridad.value;
+
+  if (nombre.length == 0 || fecha.length == 0 || prioridad == 0) {
+    // saltar un cuadro
+    alert("No hay datos");
+  } else {
+    let tarea1 = new tarea(
+      inputNombre.value,
+      new Date(inputFecha.value),
+      selectPrioridad.value
+    );
+
+    tareas.push(tarea1);
+
+    lista.innerHTML = `<li class='form-control'>${
+      tareas[tareas.length - 1].nombre
+    }</li>`;
+    inputNombre.value = "";
+    inputFecha.value = "";
+    selectPrioridad.value = "0";
+  }
 });
