971944143326ecc394b408b6abd3e4a707b0b123
Develop and System <developandsystem@gmail.com>
2026-01-27 10:04:14 +0100

    update

--- Tema5/codigos_ces/inicial/src/app/components/repeticiones/repeticiones.ts
+++ Tema5/codigos_ces/inicial/src/app/components/repeticiones/repeticiones.ts
@@ -1,4 +1,6 @@
+import Swal from 'sweetalert2';
 import { Component } from '@angular/core';
+import { usuario } from '../../model/usuario';
 
 @Component({
   selector: 'app-repeticiones',
@@ -7,10 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './repeticiones.css',
 })
 export class Repeticiones {
+  usauriosData?: usuario[];
   usuarios: string[] = ['Borja', 'Maria', 'Juan', 'Marta', 'Pedro', 'Celia', 'Claudia'];
   nombreIntroducido: string = '';
 
   agregarUsuario() {
-    throw new Error('Method not implemented.');
+    // hay datos
+    // esta en la lista
+    // lo agrego
+    if (this.nombreIntroducido.length == 0) {
+      this.lanzarCuadro('Error', 'No hay datos en el campo', 'error');
+    } else if (this.usuarios.find((p) => p == this.nombreIntroducido) != undefined) {
+      this.lanzarCuadro('Error', 'El usuario esta en la lista', 'error');
+    } else {
+      this.usuarios.push(this.nombreIntroducido);
+      this.lanzarCuadro('Existo', 'Usuario agregado correctamente', 'success');
+    }
+  }
+  lanzarCuadro(titulo: string, texto: string, icono: string) {
+    Swal.fire({
+      title: titulo,
+      text: texto,
+    });
   }
 }
