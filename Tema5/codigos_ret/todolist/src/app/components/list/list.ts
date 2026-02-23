b25e48b65133130d6ca188fd764e9e3a769c9433
Develop and System <developandsystem@gmail.com>
2026-02-23 17:13:34 +0100

    update

--- Tema5/codigos_ret/todolist/src/app/components/list/list.ts
+++ Tema5/codigos_ret/todolist/src/app/components/list/list.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Tarea } from '../../model/tarea';
 import { TareasGestor } from '../../services/tareas-gestor';
 import { CommonModule } from '@angular/common';
+import { ImagenesPipe } from '../../pipes/imagenes-pipe';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ImagenesPipe],
   templateUrl: './list.html',
   styleUrl: './list.css',
 })
--- Tema5/codigos_ret/todolist/src/app/pipes/imagenes-pipe.ts
+++ Tema5/codigos_ret/todolist/src/app/pipes/imagenes-pipe.ts
@@ -5,7 +5,16 @@ import { Tarea } from '../model/tarea';
   name: 'imagenes',
 })
 export class ImagenesPipe implements PipeTransform {
-  transform(value: Tarea, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, ...args: unknown[]): string {
+    switch (value) {
+      case 1:
+        return 'https://img.icons8.com/color/1200/low-priority.jpg';
+      case 2:
+        return 'https://img.icons8.com/color/1200/medium-priority.jpg';
+      case 3:
+        return 'https://www.svgrepo.com/show/474865/high-priority.svg';
+      default:
+        return 'null';
+    }
   }
 }
