d2c4fd507f26d6523a6fb0d606a2f9a1665562af
Develop and System <developandsystem@gmail.com>
2026-09-16 09:47:14 +0200

    update

--- todolist/src/main/java/MainFile.java
+++ todolist/src/main/java/MainFile.java
@@ -1,10 +1,25 @@
-import java.io.File;
+import java.io.*;
 
 public class MainFile {
 
     public static void main(String[] args) {
-        File file = new File("src/main/java/files/other/test");
-        file.createNewFile();
+        File file = new File("src/main/java/files/task.txt");
+        // BufferedWriter bufferedWriter = null;
+        FileWriter fileWriter = null;
+        PrintWriter printWriter = null;
+        try {
+            file.createNewFile();
+            fileWriter = new FileWriter(file);
+            fileWriter.write("Tareas");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException | NullPointerException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         System.out.println(file.getAbsolutePath());
     }
